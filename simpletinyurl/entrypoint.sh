c6fdc593042181ca112a738c4023c25b0f1efe1b
Benny <ben.jaya87@gmail.com>
2022-10-22 00:38:08 +0800

    update dockerfile, run django as non-root user

--- simpletinyurl/entrypoint.sh
+++ simpletinyurl/entrypoint.sh
@@ -3,8 +3,8 @@
 # Wait for Postgres
 if [ $DJANGO_DB_TYPE = "postgres" ]
 then
-    apk update
-    apk add postgresql-client --no-cache 
+    sudo apk update
+    sudo apk add postgresql-client --no-cache 
     until [ "$(PGPASSWORD=$DJANGO_DB_PASSWORD psql -U $DJANGO_DB_USER -h $DJANGO_DB_HOST -tAc 'select 1' -d $DJANGO_DB_NAME || echo 0 )" = '1' ]; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
